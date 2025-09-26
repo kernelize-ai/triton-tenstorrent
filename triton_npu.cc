fb1d894cac0c5a7e1919dc22f840e2d05eff9730
Simon Waters <simon@kernelize.ai>
2025-09-26 15:59:46 -0500

    [TT] Added CoreSpecialize pass (#3)
    * [TT] Added CoreSpecialize pass
    - specialize kernel for 3 variants:
      - Load data-mover: replace load with async_copy, remove all else
      - Compute: replace loads with local_load, stores with local_store
      - Store data-mover: replace store input with local_load
    
    * - added async_copy_local_to_global (disabled until upstream support)
    - create local_allocs first, then clone
      - todo: add identifier to correlate
    
    * - use only load/store and local_load/local_store
    - annotate each local_alloc with an alloc_idx
    
    * - format and cleanup

--- triton_npu.cc
+++ triton_npu.cc
@@ -27,6 +27,9 @@ std::string getDefaultTargerOrProcessTriple() {
 }
 
 void init_triton_npu_passes_ttgpuir(py::module &&m) {
+  m.def("add_core_specialize", [](mlir::PassManager &pm) {
+    pm.addPass(mlir::triton::npu::createCoreSpecialize());
+  });
   m.def("add_to_llvmir", [](mlir::PassManager &pm) {
     pm.addPass(mlir::triton::npu::createConvertTritonCPUToLLVMPass());
   });
