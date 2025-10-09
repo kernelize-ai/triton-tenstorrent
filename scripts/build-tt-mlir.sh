1104497a037701a07db732a03225dacdd4f178b0
Alex Baden <alex@kernelize.ai>
2025-10-09 13:33:25 -0400

    Add pybind11 dir to tt-mlir cmake configure

--- scripts/build-tt-mlir.sh
+++ scripts/build-tt-mlir.sh
@@ -73,6 +73,7 @@ else
         -DTT_RUNTIME_ENABLE_TTNN=OFF \
         -DTT_RUNTIME_ENABLE_TTMETAL=OFF \
         -DTTMLIR_ENABLE_RUNTIME_TESTS=OFF \
-        -Dnanobind_DIR="$(python -c 'import os, nanobind; print(os.path.join(os.path.dirname(nanobind.__file__), "cmake"))')"
+        -Dnanobind_DIR="$(python -c 'import os, nanobind; print(os.path.join(os.path.dirname(nanobind.__file__), "cmake"))')" \
+        -Dpybind11_DIR="$(python -c 'import os, pybind11; print(os.path.join(os.path.dirname(pybind11.__file__), "share", "cmake", "pybind11"))')"
     cmake --build build
 fi
