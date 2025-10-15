00066ebea58ffa3ed8d21df26cd7a543aa791ea4
Alex Baden <alex@kernelize.ai>
2025-10-15 10:05:50 -0400

    Introduce d2m.generic 3/?

--- npu/lib/TritonNPUToTenstorrent/ConvertMathToD2M.cpp
+++ npu/lib/TritonNPUToTenstorrent/ConvertMathToD2M.cpp
@@ -19,6 +19,8 @@ namespace npu {
 #define GEN_PASS_DEF_CONVERTMATHTOD2M
 #include "npu/include/TritonNPUToTenstorrent/Passes.h.inc"
 
+using namespace tt;
+
 struct ConvertAddOp : public OpConversionPattern<arith::AddFOp> {
   using OpConversionPattern<arith::AddFOp>::OpConversionPattern;
 
@@ -29,6 +31,86 @@ struct ConvertAddOp : public OpConversionPattern<arith::AddFOp> {
     auto loc = addOp.getLoc();
 
     auto tensorTy = cast<RankedTensorType>(addOp.getType());
+
+    // TODO:
+    // 1. insert d2m.to_layout for the lhs and rhs
+    // 2. allocate the output and d2m.to_layout it
+    // 3. introduce d2m.generic which takes the to_layout outputs as inputs
+    // 4. add d2m.tile_add into the d2m.generic
+    // 5. replace op with d2m.generic output(s)
+
+#if 1
+    auto typeConverter = getTypeConverter();
+    auto retType =
+        cast<RankedTensorType>(typeConverter->convertType(addOp.getType()));
+
+    SmallVector<Value> inputs = {adaptor.getLhs(), adaptor.getRhs()};
+
+    for (auto v : inputs)
+      llvm::errs() << "input: " << v << " of type " << v.getType() << "\n";
+
+    llvm::errs() << "retType: " << retType << "\n";
+
+    // Tilize the LHS and RHS inputs using d2m.to_layout
+    auto getTiledInput = [&](Value v) -> Value {
+      auto tensorType = cast<RankedTensorType>(v.getType());
+      Type elementType = tensorType.getElementType();
+
+      constexpr std::array<int64_t, 2> defaultShape =
+          tt::ttcore::TileType::getDefaultShape();
+      SmallVector<int64_t> tileShape{defaultShape[0], defaultShape[1]};
+      Type tiledElementType = tt::ttcore::TileType::get(elementType, tileShape);
+
+      llvm::errs() << "tileShape = " << tileShape[0] << " x " << tileShape[1]
+                   << "\n";
+      llvm::errs() << "tiledElementType = " << tiledElementType << "\n";
+
+      SmallVector<int64_t> logicalShape =
+          llvm::to_vector(tensorType.getShape());
+      tt::ttcore::MemorySpace memSpace = tt::ttcore::MemorySpace::DeviceL1;
+
+      // create metal layout
+      tt::ttcore::MetalLayoutAttr layout = ttcore::MetalLayoutAttr::get(
+          rewriter.getContext(), logicalShape, targetSquareGridShape,
+          ttcore::OOBVal::Undef, memSpace, ttcore::TensorMemoryLayout::Sharded);
+
+      // Get raw, unsharded physical shape.
+      SmallVector<int64_t> unshardedShape = layout.getPhysicalShape(tileShape);
+      llvm::errs() << "unshardedShape = ";
+      for (auto s : unshardedShape)
+        llvm::errs() << s << " ";
+      llvm::errs() << "\n";
+
+      // Calculate optimal grid for given physical shape.
+      llvm::SmallVector<int64_t> optimalGrid =
+          computeOptimalGrid(unshardedShape);
+      llvm::errs() << "optimalGrid = ";
+      for (auto g : optimalGrid)
+        llvm::errs() << g << " ";
+      llvm::errs() << "\n";
+
+      // Get optimal sharded, on-device shape.
+      llvm::SmallVector<int64_t> shardedShape =
+          layout.getDeviceShape(optimalGrid, tileShape);
+      llvm::errs() << "shardedShape = ";
+      for (auto s : shardedShape)
+        llvm::errs() << s << " ";
+      llvm::errs() << "\n";
+
+      auto emptyOp = rewriter.create<d2m::EmptyOp>(v.getLoc(), shardedShape,
+                                                   elementType, layout);
+      return rewriter.create<d2m::ToLayoutOp>(v.getLoc(), v, emptyOp)
+          ->getResult(0);
+    };
+    auto lhsTiled = getTiledInput(adaptor.getLhs());
+    auto rhsTiled = getTiledInput(adaptor.getRhs());
+
+    // tilize the output type, allocate using d2m.empty, and tilize using d2m.to_layout 
+    
+
+    assert(false && "TODO");
+#else
+
     SmallVector<mlir::AffineMap> indexingMap = {
         rewriter.getMultiDimIdentityMap(tensorTy.getRank())};
     mlir::Attribute iteratorType = tt::ttcore::IteratorTypeAttr::get(
@@ -110,8 +192,9 @@ struct ConvertAddOp : public OpConversionPattern<arith::AddFOp> {
                 mlir::ValueRange bbArgs) {
               mlir::Value yield;
 
-              llvm::errs() << "Creating tile add op with input types " << bbArgs[0].getType()
-                           << " and " << bbArgs[1].getType() << "\n";
+              llvm::errs() << "Creating tile add op with input types "
+                           << bbArgs[0].getType() << " and "
+                           << bbArgs[1].getType() << "\n";
               llvm::errs() << "and output type " << bbArgs[2].getType() << "\n";
               // For regular elementwise ops, create TileOp directly.
               yield = bbBuilder.create<tt::d2m::TileAddOp>(
@@ -134,6 +217,7 @@ struct ConvertAddOp : public OpConversionPattern<arith::AddFOp> {
     rewriter.replaceOpWithNewOp<tt::d2m::TileAddOp>(
         addOp, retType, adaptor.getLhs(), adaptor.getRhs());
 #endif
+#endif
     return success();
   }
 
@@ -154,7 +238,37 @@ struct ConvertAddOp : public OpConversionPattern<arith::AddFOp> {
     return grid;
   }
 
-  const SmallVector<size_t> targetSquareGridShape = {32};
+  // Compute optimal grid shape that works for all provided layout infos.
+  llvm::SmallVector<int64_t>
+  computeOptimalGrid(ArrayRef<int64_t> physicalShape) const {
+    llvm::SmallVector<int64_t> grid;
+    grid.reserve(physicalShape.size());
+
+    assert(physicalShape.size() >= targetSquareGridShape.size());
+
+    const size_t gridRankDiff =
+        physicalShape.size() - targetSquareGridShape.size();
+    grid.assign(gridRankDiff, 1);
+
+    for (size_t i = gridRankDiff; i < physicalShape.size(); ++i) {
+      const int64_t dim = physicalShape[i];
+      assert(dim > 0);
+      // Find largest grid dimension that divides evenly.
+      for (int64_t g = targetSquareGridShape[i - gridRankDiff]; g > 0; g--) {
+        if (dim % g == 0) {
+          grid.push_back(g);
+          break;
+        }
+      }
+    }
+
+    assert(grid.size() == physicalShape.size());
+
+    return grid;
+  }
+
+  // TODO: retrieve from device info?
+  const SmallVector<int64_t> targetSquareGridShape = {1, 1};
 };
 
 struct ConvertMathTOD2MPass
@@ -173,6 +287,15 @@ struct ConvertMathTOD2MPass
     TypeConverter typeConverter;
     typeConverter.addConversion([](Type type) { return type; });
     typeConverter.addConversion([](RankedTensorType type) {
+#if 1
+      // make all tensors 2D
+      SmallVector<int64_t> shape = llvm::to_vector(type.getShape());
+      if (shape.size() == 1) {
+        shape.push_back(1);
+      }
+      return RankedTensorType::get(shape, type.getElementType(),
+                                   type.getEncoding());
+#else
       // changes the encoding from triton -> tt
       tt::ttcore::MemorySpace memSpace = tt::ttcore::MemorySpace::DeviceL1;
       tt::ttcore::TensorMemoryLayout memLayout =
@@ -180,7 +303,7 @@ struct ConvertMathTOD2MPass
       SmallVector<int64_t> deviceGridShape = {
           32,
           32}; // TODO: get from module attributes, need to populate device info
-      
+
       auto i64Ty = IntegerType::get(type.getContext(), 64);
       auto intervalTy = RankedTensorType::get({1, 2}, i64Ty);
       DenseIntElementsAttr collapsedIntervals = DenseIntElementsAttr::get(
@@ -196,6 +319,7 @@ struct ConvertMathTOD2MPass
           type.getContext(), shape, deviceGridShape, tt::ttcore::OOBVal::Undef,
           memSpace, memLayout, collapsedIntervals, dimAlignments);
       return RankedTensorType::get(shape, type.getElementType(), ttLayout);
+#endif
     });
 
     RewritePatternSet patterns(context);
