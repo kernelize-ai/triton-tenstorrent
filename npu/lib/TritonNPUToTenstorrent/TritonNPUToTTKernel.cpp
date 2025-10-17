eac03e15b179c916276efe17074da34b77ffebfe
Alex Baden <alex@kernelize.ai>
2025-10-17 17:34:51 -0400

    Lower compute function to ttkernel 2/?

--- npu/lib/TritonNPUToTenstorrent/TritonNPUToTTKernel.cpp
+++ npu/lib/TritonNPUToTenstorrent/TritonNPUToTTKernel.cpp
@@ -18,7 +18,6 @@ namespace npu {
 #define GEN_PASS_DEF_CONVERTTRITONNPUTOTTKERNEL
 #include "npu/include/TritonNPUToTenstorrent/Passes.h.inc"
 
-
 namespace {
 
 struct ConvertAddOp : public OpConversionPattern<arith::AddFOp> {
@@ -28,19 +27,27 @@ struct ConvertAddOp : public OpConversionPattern<arith::AddFOp> {
   matchAndRewrite(arith::AddFOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
     Location loc = op.getLoc();
-    
+
     auto lhs = adaptor.getLhs();
     auto rhs = adaptor.getRhs();
 
-    // create init op 
+    // create init op
     rewriter.create<ttkernel::AddTilesInitOp>(loc, lhs, rhs);
 
-    Value lhsIndex = rewriter.create<arith::ConstantOp>(loc, rewriter.getIndexType(), rewriter.getIntegerAttr(rewriter.getIndexType(), 0));
-    Value rhsIndex = rewriter.create<arith::ConstantOp>(loc, rewriter.getIndexType(), rewriter.getIntegerAttr(rewriter.getIndexType(), 1));
-    Value destIndex = rewriter.create<arith::ConstantOp>(loc, rewriter.getIndexType(), rewriter.getIntegerAttr(rewriter.getIndexType(), 2));
-    rewriter.create<ttkernel::AddTilesOp>(loc, lhs, rhs, lhsIndex, rhsIndex, destIndex);
+    Value lhsIndex = rewriter.create<arith::ConstantOp>(
+        loc, rewriter.getIndexType(),
+        rewriter.getIntegerAttr(rewriter.getIndexType(), 0));
+    Value rhsIndex = rewriter.create<arith::ConstantOp>(
+        loc, rewriter.getIndexType(),
+        rewriter.getIntegerAttr(rewriter.getIndexType(), 1));
+    Value destIndex = rewriter.create<arith::ConstantOp>(
+        loc, rewriter.getIndexType(),
+        rewriter.getIntegerAttr(rewriter.getIndexType(), 2));
+    rewriter.create<ttkernel::AddTilesOp>(loc, lhs, rhs, lhsIndex, rhsIndex,
+                                          destIndex);
 
-    op.erase();
+    if (op->use_empty())
+      op.erase();
     return success();
   }
 };
@@ -52,59 +59,69 @@ struct ConvertLocalStoreOp : public OpConversionPattern<gpu::LocalStoreOp> {
   matchAndRewrite(gpu::LocalStoreOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
     Location loc = op.getLoc();
-    
-    assert(false && "TODO");
-    // auto tile = adaptor.getTile();
-    // auto cb = adaptor.getCb();
-    // auto index = adaptor.getIndex();
 
+    auto dst = adaptor.getDst();
+    llvm::errs() << "converted dst = " << dst << "\n";
 
-    // rewriter.create<ttkernel::StoreTileToCBOp>(loc, tile, cb, index);
+    Value destRegisterIndex = rewriter.create<arith::ConstantOp>(
+        loc, rewriter.getIndexType(),
+        rewriter.getIntegerAttr(rewriter.getIndexType(), 2));
+    Value outIndex = rewriter.create<arith::ConstantOp>(
+        loc, rewriter.getIndexType(),
+        rewriter.getIntegerAttr(rewriter.getIndexType(), 0));
+    rewriter.create<ttkernel::PackTileOp>(loc, destRegisterIndex, dst, outIndex,
+                                          /*outOfOrder=*/true);
 
-    // op.erase();
-    return failure();
+    auto srcOp = op.getSrc().getDefiningOp();
+    op.erase();
+
+    // erase any unused defining ops (tile commit relationship is implicit
+    // through cb refs)
+    if (srcOp && srcOp->use_empty())
+      srcOp->erase();
+
+    return success();
   }
 };
 
-}
-
+} // namespace
 
 struct ConvertTritonNPUToTTKernelPass
-    : public impl::ConvertTritonNPUToTTKernelBase<ConvertTritonNPUToTTKernelPass> {
+    : public impl::ConvertTritonNPUToTTKernelBase<
+          ConvertTritonNPUToTTKernelPass> {
   void runOnOperation() override {
     MLIRContext *context = &getContext();
     ModuleOp mod = getOperation();
 
     mlir::ConversionTarget target{*context};
     target.addLegalDialect<tt::ttkernel::TTKernelDialect>();
-    
-#if 0
-    target.addDynamicallyLegalOp<arith::AddFOp>([](arith::AddFOp op) {
-      return !isa<RankedTensorType>(op.getType());
-    });
-#endif 
+    target.addLegalDialect<arith::ArithDialect>();
+
+#if 1
+    target.addDynamicallyLegalOp<arith::AddFOp>(
+        [](arith::AddFOp op) { return !isa<RankedTensorType>(op.getType()); });
+#endif
 
     target.addDynamicallyLegalOp<gpu::LocalStoreOp>([](gpu::LocalStoreOp op) {
-        auto funcOp = op->getParentOfType<triton::FuncOp>();
-        assert(funcOp && "expected triton::funcOp parent");
-        StringRef funcName = funcOp.getSymName();
-        return !funcName.ends_with("__compute");
+      auto funcOp = op->getParentOfType<triton::FuncOp>();
+      assert(funcOp && "expected triton::funcOp parent");
+      StringRef funcName = funcOp.getSymName();
+      return !funcName.ends_with("__compute");
     });
 
     TypeConverter typeConverter;
     typeConverter.addConversion([](Type type) { return type; });
 
     mlir::RewritePatternSet patterns(context);
-    // Going to have to lower local load and store first... 
+    // Going to have to lower local load and store first...
     patterns.add<ConvertLocalStoreOp>(typeConverter, patterns.getContext());
-    // patterns.add<ConvertAddOp>(typeConverter, patterns.getContext());
+    patterns.add<ConvertAddOp>(typeConverter, patterns.getContext());
 
     if (applyPartialConversion(mod, target, std::move(patterns)).failed())
       signalPassFailure();
-
   }
 };
 
-}
-}
-}
+} // namespace npu
+} // namespace triton
+} // namespace mlir
