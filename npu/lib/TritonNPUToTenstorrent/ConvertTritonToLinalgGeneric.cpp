230cc2a1828b19fcc535174d73fb02b5d11add54
Alex Baden <alex@kernelize.ai>
2025-10-16 22:10:54 -0400

    Convert triton elementwise ops to linalg 2/?

--- npu/lib/TritonNPUToTenstorrent/ConvertTritonToLinalgGeneric.cpp
+++ npu/lib/TritonNPUToTenstorrent/ConvertTritonToLinalgGeneric.cpp
@@ -17,6 +17,50 @@ using namespace triton;
 
 namespace {
 
+struct MakeRangeToLinalgGeneric : public OpConversionPattern<triton::MakeRangeOp> {
+    using OpConversionPattern<triton::MakeRangeOp>::OpConversionPattern;
+
+    LogicalResult matchAndRewrite(triton::MakeRangeOp makeRangeOp, OpAdaptor adaptor, ConversionPatternRewriter &rewriter) const override {
+        auto loc = makeRangeOp.getLoc();
+        auto resTy = dyn_cast<RankedTensorType>(makeRangeOp.getResult().getType());
+        if (!resTy || resTy.getRank() != 1) return failure();
+
+        Type elemTy = resTy.getElementType();
+        if (!elemTy.isSignlessInteger(32)) {
+            return rewriter.notifyMatchFailure(makeRangeOp, "expect i32 element type in skeleton");
+        }
+
+        // Build empty destination
+        SmallVector<OpFoldResult> sizes = { rewriter.getIndexAttr(resTy.getDimSize(0)) };
+        Value init = rewriter.create<tensor::EmptyOp>(loc, sizes, elemTy, resTy.getEncoding());
+
+        // linalg.generic: outs-only, 1 parallel loop, map (i)->(i)
+    SmallVector<AffineMap> maps = { AffineMap::get(/*dimCount=*/1, /*symCount=*/0,
+                              rewriter.getAffineDimExpr(0)) };
+    SmallVector<mlir::utils::IteratorType> iters = { mlir::utils::IteratorType::parallel };
+
+    auto generic = rewriter.create<linalg::GenericOp>(
+        loc, TypeRange{resTy}, /*inputs=*/ValueRange{}, /*outputs=*/ValueRange{init},
+        /*indexing_maps=*/maps,
+        /*iterator_types=*/iters,
+        [&](OpBuilder &b, Location loc, ValueRange args) {
+          // index i
+          Value i = b.create<linalg::IndexOp>(loc, 0);
+          // start/end are attributes on tt.make_range
+          int64_t start = makeRangeOp.getStart(); // adjust to your accessor
+          Value startIdx = b.create<arith::ConstantIndexOp>(loc, start);
+          Value sumIdx = b.create<arith::AddIOp>(loc, i, startIdx);
+          Value val = b.create<arith::IndexCastOp>(loc, elemTy, sumIdx);
+          b.create<linalg::YieldOp>(loc, val);
+        });
+
+    rewriter.replaceOp(makeRangeOp, generic.getResult(0));
+    return success();
+
+    }
+
+};
+
 struct SplatToLinalgFill : public OpConversionPattern<triton::SplatOp> {
   using OpConversionPattern<triton::SplatOp>::OpConversionPattern;
 
@@ -59,14 +103,16 @@ struct ConvertTritonToLinalgGenericPass : public impl::ConvertTritonToLinalgGene
     ConversionTarget target(*context);
     target.addLegalDialect<linalg::LinalgDialect>();
     target.addLegalDialect<tensor::TensorDialect>();
+    target.addLegalDialect<arith::ArithDialect>();
     target.addIllegalOp<triton::SplatOp>();
-    // target.addIllegalOp<triton::MakeRangeOp>();
+    target.addIllegalOp<triton::MakeRangeOp>();
 
     TypeConverter typeConverter;
     typeConverter.addConversion([](Type type) { return type; });
 
     RewritePatternSet patterns(context);
     patterns.add<SplatToLinalgFill>(typeConverter, patterns.getContext());
+    patterns.add<MakeRangeToLinalgGeneric>(typeConverter, patterns.getContext());
 
     if (applyPartialConversion(mod, target, std::move(patterns)).failed())
       signalPassFailure();
