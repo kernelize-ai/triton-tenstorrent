0032f12fc25b2c5973c77c6a0726b13f893a8059
Alex Baden <alex@kernelize.ai>
2025-11-02 13:14:27 -0500

    add missing file

---
+++ npu/lib/TritonNPUToTenstorrent/PatternTritonNPUToTenstorrent.h
@@ -0,0 +1,18 @@
+#ifndef TRITON_NPU_CONVERSION_TRITONNPU_TO_TENSTORRENT_PATTERNS_H
+#define TRITON_NPU_CONVERSION_TRITONNPU_TO_TENSTORRENT_PATTERNS_H
+
+#include "mlir/Transforms/DialectConversion.h"
+
+namespace mlir {
+namespace triton {
+namespace npu {
+
+void populateFuncOpConversionPattern(TypeConverter &typeConverter,
+                                     RewritePatternSet &patterns,
+                                     PatternBenefit benefit);
+
+}
+} // namespace triton
+} // namespace mlir
+
+#endif // TRITON_NPU_CONVERSION_TRITONNPU_TO_TENSTORRENT_PATTERNS_H
