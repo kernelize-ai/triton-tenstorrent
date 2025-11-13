edfdf32be2a26fba7a4f95bf5fd26fa817f1316a
Alex Baden <alex@kernelize.ai>
2025-11-13 02:25:00 +0000

    Rename PropagateRegisterIndices to PropagateTileEncoding

--- triton_cpu.cc
+++ triton_cpu.cc
@@ -61,7 +61,7 @@ void init_triton_npu_passes_tenstorrent(py::module &&m) {
   });
   m.def("add_propagate_register_indices", [](mlir::PassManager &pm) {
     pm.addPass(
-        mlir::triton::npu::createTritonTenstorrentPropagateRegisterIndices());
+        mlir::triton::npu::createTritonTenstorrentPropagateTileEncoding());
   });
   m.def("add_ptr_rotate", [](mlir::PassManager &pm) {
     pm.addPass(mlir::triton::npu::createTritonTenstorrentPtrRotate());
