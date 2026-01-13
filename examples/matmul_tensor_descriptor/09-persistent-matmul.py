5be82053b177efa773df7da444edf99999c749c7
Simon Waters <simon@kernelize.ai>
2026-01-13 12:54:49 -0600

    [TT-METAL] Launch Triton kernels thru nexus on tt-metal runtime (#76)
    * [TT-METAL] Launch Triton kernels thru nexus on tt-metal runtime
    - creates CB for each tensor passed to kernel
    - hardcoded to depth=1. TODO: use tuning config to specify
    - add support for device buffers thru triton driver api. TODO: use privateuse1
    
    * - only import nexus when used (fixes off-line compiler)
    
    * - format
    
    * - move nexus device query to try block
    
    * - send device to CpuUtils
    
    * - replace tt-metal with tenstorrent (except for nexus)
    
    * [TT] Add nexus support for TensorDescriptor
    
    * [Driver] Updated to support MakePersistent
    
    * - format
    
    * - moved device lookup

--- examples/matmul_tensor_descriptor/09-persistent-matmul.py
+++ examples/matmul_tensor_descriptor/09-persistent-matmul.py
@@ -261,7 +261,10 @@ def validate(M, N, K, dtype):
     #b = b.T.contiguous()
 
     naive_result = torch_matmul(a, b.T).to(torch.float16)
-    run_test(naive_result, matmul_tma(a, b, False), "TMA", enabled=True)
+    triton_result = matmul_tma(a, b, False)
+    print(f"triton_result: {triton_result}")
+    print(f"naive_result: {naive_result}")
+
     """
     run_test(naive_result, torch_matmul, a, b, "Torch", enabled=dtype == torch.float16)
     run_test(naive_result, cublas_matmul, a, b, "cuBLAS", enabled=cublas is not None)
@@ -316,6 +319,7 @@ if __name__ == "__main__":
         torch.manual_seed(0)
 
         validate(64, 32, 64, dtype)
+        validate(1024, 1024, 1024, dtype)
         
         """
         validate(8192, 8192, args.K_range[0], dtype)
