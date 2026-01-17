e98ae6f1b506c0b8d52e7d2d8da8d10a52286167
Alex Baden <alex@kernelize.ai>
2026-01-17 04:19:54 +0000

    update reader/writer example kernels to use new tiled load scheme

--- examples/matmul_tensor_descriptor/kernels/dataflow/writer.cpp
+++ examples/matmul_tensor_descriptor/kernels/dataflow/writer.cpp
@@ -4,46 +4,46 @@
 #include "internal/firmware_common.h"
 #include "api/dataflow/dataflow_api.h"
 void kernel_main() {
-  int32_t v1 = 1;
-  bool v2 = true;
-  int32_t v3 = 0;
-  int32_t v4 = 2;
-  int32_t v5 = get_arg_val<uint32_t>(20);
-  int32_t v6 = get_arg_val<uint32_t>(22);
-  int32_t v7 = get_arg_val<uint32_t>(30);
-  int32_t v8 = get_arg_val<uint32_t>(31);
-  DataFormat v9 = get_dataformat(get_compile_time_arg_val(2));
-  int32_t v10 = get_tile_size(get_compile_time_arg_val(2));
-  InterleavedAddrGenFast<true> v11;
-  v11.bank_base_address = v5;
-  v11.page_size = v10;
-  v11.data_format = v9;
-  InterleavedAddrGenFast<true> v12 = v11;
-  int32_t v13 = get_arg_val<uint32_t>(34);
-  int32_t v14 = get_arg_val<uint32_t>(33);
-  for (int32_t i15 = v14; i15 < v13; i15 += v1) {
+  int32_t v1 = 64;
+  int32_t v2 = 32;
+  int32_t v3 = 1;
+  bool v4 = true;
+  int32_t v5 = 2;
+  int32_t v6 = 0;
+  int32_t v7 = get_arg_val<uint32_t>(20);
+  int32_t v8 = get_arg_val<uint32_t>(22);
+  int32_t v9 = get_arg_val<uint32_t>(30);
+  int32_t v10 = get_arg_val<uint32_t>(31);
+  DataFormat v11 = get_dataformat(get_compile_time_arg_val(2));
+  int32_t v12 = get_tile_size(get_compile_time_arg_val(2));
+  InterleavedAddrGenFast<true> v13;
+  v13.bank_base_address = v7;
+  v13.page_size = v12;
+  v13.data_format = v11;
+  InterleavedAddrGenFast<true> v14 = v13;
+  int32_t v15 = get_arg_val<uint32_t>(34);
+  int32_t v16 = get_arg_val<uint32_t>(33);
+  int32_t v17 = (int32_t) ((uint32_t) v10 + (uint32_t) 63) / v1;
+  for (int32_t i18 = v16; i18 < v15; i18 += v3) {
+    int32_t v19 = i18 / v17;
+    int32_t v20 = (int32_t) ((uint32_t) ((int32_t) ((uint32_t) v9 + (uint32_t) 31) / v2) - (uint32_t) v19) < v3 ? (int32_t) ((uint32_t) ((int32_t) ((uint32_t) v9 + (uint32_t) 31) / v2) - (uint32_t) v19) : v3;
     {
     DeviceZoneScopedN("cb_wait_front");
-    cb_wait_front(get_compile_time_arg_val(2), v4);
+    cb_wait_front(get_compile_time_arg_val(2), v5);
     }
-    int32_t v16 = get_read_ptr(get_compile_time_arg_val(2));
-    int32_t v17;
-    int32_t v18;
-    v17 = v16;
-    v18 = (int32_t) ((uint32_t) ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) (i15 / ((int32_t) ((uint32_t) v8 + (uint32_t) 63) / 64)) + (uint32_t) (i15 % ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) v7 + (uint32_t) 31) / 32) - (uint32_t) (i15 / ((int32_t) ((uint32_t) v8 + (uint32_t) 63) / 64))) < v1 ? (int32_t) ((uint32_t) ((int32_t) ((uint32_t) v7 + (uint32_t) 31) / 32) - (uint32_t) (i15 / ((int32_t) ((uint32_t) v8 + (uint32_t) 63) / 64))) : v1)))) * (uint32_t) 32) / 32) * (uint32_t) (v6 != (int32_t) ((uint32_t) (v6 / 64) * (uint32_t) 64) & v6 < v3 == false ? (int32_t) ((uint32_t) (v6 / 64) + (uint32_t) v1) : v6 / 64))) + (uint32_t) ((int32_t) ((uint32_t) ((i15 % ((int32_t) ((uint32_t) v8 + (uint32_t) 63) / 64)) / ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) v7 + (uint32_t) 31) / 32) - (uint32_t) (i15 / ((int32_t) ((uint32_t) v8 + (uint32_t) 63) / 64))) < v1 ? (int32_t) ((uint32_t) ((int32_t) ((uint32_t) v7 + (uint32_t) 31) / 32) - (uint32_t) (i15 / ((int32_t) ((uint32_t) v8 + (uint32_t) 63) / 64))) : v1)) * (uint32_t) 64) / 64))) * (uint32_t) 2048)) + (uint32_t) ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) (i15 / ((int32_t) ((uint32_t) v8 + (uint32_t) 63) / 64)) + (uint32_t) (i15 % ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) v7 + (uint32_t) 31) / 32) - (uint32_t) (i15 / ((int32_t) ((uint32_t) v8 + (uint32_t) 63) / 64))) < v1 ? (int32_t) ((uint32_t) ((int32_t) ((uint32_t) v7 + (uint32_t) 31) / 32) - (uint32_t) (i15 / ((int32_t) ((uint32_t) v8 + (uint32_t) 63) / 64))) : v1)))) * (uint32_t) 32)) % (uint32_t) 32)) * (uint32_t) 64)) + (uint32_t) ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) ((i15 % ((int32_t) ((uint32_t) v8 + (uint32_t) 63) / 64)) / ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) v7 + (uint32_t) 31) / 32) - (uint32_t) (i15 / ((int32_t) ((uint32_t) v8 + (uint32_t) 63) / 64))) < v1 ? (int32_t) ((uint32_t) ((int32_t) ((uint32_t) v7 + (uint32_t) 31) / 32) - (uint32_t) (i15 / ((int32_t) ((uint32_t) v8 + (uint32_t) 63) / 64))) : v1)) * (uint32_t) 64)) % (uint32_t) 64)))))) * (uint32_t) v4)) / (uint32_t) v10);
-    for (int32_t j19 = v3; j19 < v4; j19 += v1) {
-      int32_t v20 = v17;
-      int32_t v21 = v18;
-      uint64_t temp_192 = v12.get_noc_addr(v21, v3);
-      noc_async_write(v20, temp_192, v10);
-      v17 = (int32_t) ((uint32_t) v20 + (uint32_t) v10);
-      v18 = (int32_t) ((uint32_t) v21 + (uint32_t) v1);
-    }
+    int32_t v21 = (int32_t) ((uint32_t) ((i18 % v17) / v20) * (uint32_t) v1) / v2;
+    int32_t v22 = get_read_ptr(get_compile_time_arg_val(2));
+    int32_t v23 = (int32_t) ((uint32_t) ((int32_t) ((uint32_t) ((int32_t) ((uint32_t) v19 + (uint32_t) (i18 % v20))) * (uint32_t) v2) / v2) * (uint32_t) (v8 != (int32_t) ((uint32_t) (v8 / v2) * (uint32_t) v2) & v8 < v6 == false ? (int32_t) ((uint32_t) (v8 / v2) + (uint32_t) v3) : v8 / v2));
+    uint64_t temp_143 = v14.get_noc_addr((int32_t) ((uint32_t) v23 + (uint32_t) v21), v6);
+    noc_async_write(v22, temp_143, v12);
+    int32_t v24 = (int32_t) ((uint32_t) v22 + (uint32_t) v12);
+    uint64_t temp_160 = v14.get_noc_addr((int32_t) ((uint32_t) v23 + (uint32_t) ((int32_t) ((uint32_t) v21 + (uint32_t) v3))), v6);
+    noc_async_write(v24, temp_160, v12);
     {
     DeviceZoneScopedN("noc_async_write_barrier");
     noc_async_write_barrier();
     }
-    cb_pop_front(get_compile_time_arg_val(2), v4);
+    cb_pop_front(get_compile_time_arg_val(2), v5);
   }
   return;
 }
