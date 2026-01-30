56ffb7d0cd7cbce1c4557cdaf3cd5fd7cde1d8b6
Alex Baden <alex@kernelize.ai>
2026-01-30 19:32:08 +0000

    pick better block sizes contd (64x64x64 still winning)

--- examples/matmul_tensor_descriptor/matmul_multi_core_tma.cpp
+++ examples/matmul_tensor_descriptor/matmul_multi_core_tma.cpp
@@ -94,8 +94,8 @@ void matmul_multi_core(
         TILE_HW);
 
     uint32_t BM = 64;  // block M in elements
-    uint32_t BN = 128;  // block N in elements
-    uint32_t BK = 512;  // block K in elements
+    uint32_t BN = 64;  // block N in elements
+    uint32_t BK = 64;  // block K in elements
 
     TT_ASSERT(BM % TILE_HEIGHT == 0 && BN % TILE_WIDTH == 0 && BK % TILE_WIDTH == 0,
           "Block sizes must be tile-multiple (32).");
@@ -158,7 +158,7 @@ void matmul_multi_core(
         M,
         N,
         Mt * Nt);
-
+        return;
     // Create DRAM buffers for input and output matrices (replicated per device across the mesh).
     // We allocate DRAM buffers for the input matrices and output matrix.
     // Setting page_size to single_tile_size is the most common configuration for memory buffers in Metalium
