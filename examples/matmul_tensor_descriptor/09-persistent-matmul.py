0d8e0b3f812ad67a7ff130637b77d88c07904f5e
Alex Baden <alex@kernelize.ai>
2026-02-05 21:08:53 +0000

    only one mcast for A

--- examples/matmul_tensor_descriptor/09-persistent-matmul.py
+++ examples/matmul_tensor_descriptor/09-persistent-matmul.py
@@ -76,11 +76,11 @@ HAS_WARP_SPECIALIZE = supports_ws() and HAS_TENSOR_DESC
 
 def matmul_get_configs(pre_hook=None):
     return [
-        triton.Config({'BLOCK_SIZE_M': BM, 'BLOCK_SIZE_N': BN, "BLOCK_SIZE_K": BK, "GROUP_SIZE_M": 2}, num_stages=s,
+        triton.Config({'BLOCK_SIZE_M': BM, 'BLOCK_SIZE_N': BN, "BLOCK_SIZE_K": BK, "GROUP_SIZE_M": 1}, num_stages=s,
                       num_warps=w, pre_hook=pre_hook)
-        for BM in [32]
-        for BN in [128]
-        for BK in [512]
+        for BM in [64]
+        for BN in [64]
+        for BK in [64]
         for s in ([1])
         for w in [1]
     ]
