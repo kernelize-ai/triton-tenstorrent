b783a70480a62698e86c8fb7532bf687ed4766c9
Alex Baden <alex@kernelize.ai>
2026-02-17 15:16:10 +0000

    baseline after tt-mlir update (via rebase)

--- examples/matmul_tensor_descriptor/kernels/compute/matmul_triton.cpp
+++ examples/matmul_tensor_descriptor/kernels/compute/matmul_triton.cpp
@@ -43,6 +43,33 @@ inline uint32_t float_to_bits(float f) { uint32_t r; __builtin_memcpy(&r, &f, si
 #define REDUCE_OP PoolType::SUM
 #define REDUCE_DIM ReduceDim::REDUCE_COL
 #include "api/compute/reduce.h"
+
+// SPDX-FileCopyrightText: (c) 2025 Tenstorrent AI ULC
+//
+// SPDX-License-Identifier: Apache-2.0
+
+#ifndef TTMLIR_TARGET_TTKERNEL_LLKS_EXPERIMENTAL_MATMUL_LLKS_H
+#define TTMLIR_TARGET_TTKERNEL_LLKS_EXPERIMENTAL_MATMUL_LLKS_H
+
+namespace experimental {
+
+ALWI void matmul_block(uint32_t in0_cb_id, uint32_t in1_cb_id,
+                       uint32_t in0_tile_index, uint32_t in1_tile_index,
+                       uint32_t idst, const uint32_t transpose, uint32_t ct_dim,
+                       uint32_t rt_dim, uint32_t kt_dim, uint32_t nt_dim) {
+
+  for (uint32_t i = 0; i < kt_dim; i++) {
+    ckernel::matmul_block(in0_cb_id, in1_cb_id, in0_tile_index, in1_tile_index,
+                          idst, transpose, ct_dim, rt_dim, kt_dim);
+    in0_tile_index++;
+    in1_tile_index += nt_dim;
+  }
+}
+
+} // namespace experimental
+
+#endif
+
 void kernel_main() {
   size_t v1 = 0;
   int32_t v2 = 0;
