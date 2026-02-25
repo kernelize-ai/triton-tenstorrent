db992f7572250d8cf478d6b06f0d95b868e00cf3
Alex Baden <alex@kernelize.ai>
2026-02-25 21:02:12 +0000

    update fused example with best working config

--- examples/matmul_fused/kernels/compute/matmul_triton.cpp
+++ examples/matmul_fused/kernels/compute/matmul_triton.cpp
@@ -54,17 +54,29 @@ void kernel_main() {
   size_t v8 = 7;
   int32_t v9 = 0;
   int32_t v10 = 1;
-  int32_t v11 = 4;
-  int32_t v12 = 2;
-  int32_t v13 = 3;
-  int32_t v14 = get_common_arg_val<uint32_t>(42);
+  int32_t v11 = 16;
+  int32_t v12 = 8;
+  int32_t v13 = 9;
+  int32_t v14 = 2;
+  int32_t v15 = 10;
+  int32_t v16 = 3;
+  int32_t v17 = 11;
+  int32_t v18 = 4;
+  int32_t v19 = 12;
+  int32_t v20 = 5;
+  int32_t v21 = 13;
+  int32_t v22 = 6;
+  int32_t v23 = 14;
+  int32_t v24 = 7;
+  int32_t v25 = 15;
+  int32_t v26 = get_common_arg_val<uint32_t>(42);
   mm_init(get_compile_time_arg_val(0), get_compile_time_arg_val(1), get_compile_time_arg_val(3), v9);
-  int32_t v15 = get_arg_val<uint32_t>(v2);
-  int32_t v16 = get_arg_val<uint32_t>(v1);
-  for (int32_t i17 = v16; i17 < v15; i17 += v10) {
+  int32_t v27 = get_arg_val<uint32_t>(v2);
+  int32_t v28 = get_arg_val<uint32_t>(v1);
+  for (int32_t i29 = v28; i29 < v27; i29 += v10) {
     tile_regs_acquire();
     mm_init_short(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v9);
-    for (int32_t j18 = v9; j18 < ((int32_t) ((uint32_t) v14 + (uint32_t) 63) / 64); j18 += v10) {
+    for (int32_t j30 = v9; j30 < ((int32_t) ((uint32_t) v26 + (uint32_t) 255) / 256); j30 += v10) {
       {
       DeviceZoneScopedN("cb_wait_front");
       cb_wait_front(get_compile_time_arg_val(0), v11);
@@ -81,25 +93,49 @@ void kernel_main() {
       matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v10, v13, v2);
       matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v13, v10, v3);
       matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v13, v13, v4);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v14, v14, v1);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v14, v15, v2);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v15, v14, v3);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v15, v15, v4);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v16, v16, v1);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v16, v17, v2);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v17, v16, v3);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v17, v17, v4);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v18, v18, v1);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v18, v19, v2);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v19, v18, v3);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v19, v19, v4);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v20, v20, v1);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v20, v21, v2);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v21, v20, v3);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v21, v21, v4);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v22, v22, v1);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v22, v23, v2);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v23, v22, v3);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v23, v23, v4);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v24, v24, v1);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v24, v25, v2);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v25, v24, v3);
+      matmul_tiles(get_compile_time_arg_val(0), get_compile_time_arg_val(1), v25, v25, v4);
       cb_pop_front(get_compile_time_arg_val(0), v11);
       cb_pop_front(get_compile_time_arg_val(1), v11);
     }
     {
     DeviceZoneScopedN("cb_wait_front");
-    cb_wait_front(get_compile_time_arg_val(2), v11);
+    cb_wait_front(get_compile_time_arg_val(2), v18);
     }
     copy_tile_init(get_compile_time_arg_val(2));
     copy_tile(get_compile_time_arg_val(2), v1, v5);
     copy_tile(get_compile_time_arg_val(2), v2, v6);
     copy_tile(get_compile_time_arg_val(2), v3, v7);
     copy_tile(get_compile_time_arg_val(2), v4, v8);
-    cb_pop_front(get_compile_time_arg_val(2), v11);
+    cb_pop_front(get_compile_time_arg_val(2), v18);
     add_binary_tile_init();
     add_binary_tile(v1, v5, v1);
     add_binary_tile(v2, v6, v2);
     add_binary_tile(v3, v7, v3);
     add_binary_tile(v4, v8, v4);
-    cb_reserve_back(get_compile_time_arg_val(3), v11);
+    cb_reserve_back(get_compile_time_arg_val(3), v18);
     tile_regs_commit();
     {
     DeviceZoneScopedN("tile_regs_wait");
@@ -107,10 +143,10 @@ void kernel_main() {
     }
     pack_tile<true>(v9, get_compile_time_arg_val(3), v9);
     pack_tile<true>(v10, get_compile_time_arg_val(3), v10);
-    pack_tile<true>(v12, get_compile_time_arg_val(3), v12);
-    pack_tile<true>(v13, get_compile_time_arg_val(3), v13);
+    pack_tile<true>(v14, get_compile_time_arg_val(3), v14);
+    pack_tile<true>(v16, get_compile_time_arg_val(3), v16);
     tile_regs_release();
-    cb_push_back(get_compile_time_arg_val(3), v11);
+    cb_push_back(get_compile_time_arg_val(3), v18);
   }
   return;
 }
--- examples/matmul_fused/matmul_fused_add.cpp
+++ examples/matmul_fused/matmul_fused_add.cpp
@@ -98,7 +98,7 @@ void matmul_multi_core(
 
     uint32_t BM = 64;  // block M in elements
     uint32_t BN = 64;  // block N in elements
-    uint32_t BK = 64;  // block K in elements
+    uint32_t BK = 256;  // block K in elements
 
     TT_ASSERT(BM % TILE_HEIGHT == 0 && BN % TILE_WIDTH == 0 && BK % TILE_WIDTH == 0,
           "Block sizes must be tile-multiple (32).");
