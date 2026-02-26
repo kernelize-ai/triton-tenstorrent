50f6fe7812a440bdde624b280007f8f78d544815
Alex Baden <alex@kernelize.ai>
2026-02-26 14:27:27 +0000

    Update to tenstorrent/tt-mlir@19d899d24

--- examples/matmul_fused/kernels/compute/matmul_triton.cpp
+++ examples/matmul_fused/kernels/compute/matmul_triton.cpp
@@ -13,6 +13,7 @@
 #include "api/compute/eltwise_binary.h"
 #include "api/compute/eltwise_binary_sfpu.h"
 #include "api/compute/compute_kernel_api.h"
+#include "api/compute/copy_dest_values.h"
 #include "api/compute/tile_move_copy.h"
 #include "api/compute/eltwise_unary/activations.h"
 #include "api/compute/eltwise_unary/eltwise_unary.h"
