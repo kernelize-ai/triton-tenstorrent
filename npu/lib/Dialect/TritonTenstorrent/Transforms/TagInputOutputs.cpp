5729de1aefdc333960010fb551f32840d2c48d41
Alex Baden <alex@kernelize.ai>
2026-06-08 12:38:47 +0000

    format fixes + debug cleanups

--- npu/lib/Dialect/TritonTenstorrent/Transforms/TagInputOutputs.cpp
+++ npu/lib/Dialect/TritonTenstorrent/Transforms/TagInputOutputs.cpp
@@ -1,6 +1,5 @@
 #include "npu/include/Dialect/TritonTenstorrent/Transforms/Passes.h"
 
-#include "mlir/Analysis/SliceAnalysis.h"
 #include "mlir/IR/BuiltinOps.h"
 #include "mlir/Pass/Pass.h"
 
@@ -32,7 +31,7 @@ public:
 
       funcOp.walk([&](Operation *op) {
         Value ptr;
-        tt::IOType desired;
+        tt::IOType desired{};
         if (auto loadOp = dyn_cast<triton::LoadOp>(op)) {
           ptr = loadOp.getPtr();
           desired = tt::IOType::INPUT;
@@ -57,21 +56,6 @@ public:
         if (failed(tagAs(idx, desired, funcOp)))
           signalPassFailure();
       });
-
-#if 0
-      // todo: delete me
-      auto &body = funcOp.getBody();
-      for (auto arg : body.front().getArguments()) {
-
-        Type argType = arg.getType();
-        if ((isa<PointerType>(argType)) || isa<TensorDescType>(argType)) {
-          if (failed(addTag(arg, funcOp))) {
-            signalPassFailure();
-            return;
-          }
-        }
-      }
-#endif
     });
   }
 
@@ -79,7 +63,7 @@ private:
   static constexpr llvm::StringLiteral kIOTypeAttrName =
       "triton_tenstorrent.io_type";
 
-  BlockArgument traceToFuncArg(Value v, triton::FuncOp funcOp) {
+  static BlockArgument traceToFuncArg(Value v, triton::FuncOp funcOp) {
     while (true) {
       if (auto blockArg = dyn_cast<BlockArgument>(v))
         return blockArg.getOwner() == &funcOp.getBody().front() ? blockArg
@@ -108,7 +92,7 @@ private:
 
   // Tag the argument with `desired`, failing if it was already tagged with a
   // conflicting io_type (i.e. used as both an input and an output).
-  LogicalResult tagAs(unsigned idx, tt::IOType desired, triton::FuncOp funcOp) {
+  static LogicalResult tagAs(unsigned idx, tt::IOType desired, triton::FuncOp funcOp) {
     if (auto existingAttr = funcOp.getArgAttr(idx, kIOTypeAttrName)) {
       auto existing = dyn_cast<tt::IOTypeAttr>(existingAttr);
       if (existing && existing.getValue() == desired)
@@ -120,115 +104,6 @@ private:
     }
     funcOp.setArgAttr(idx, kIOTypeAttrName,
                       tt::IOTypeAttr::get(funcOp.getContext(), desired));
-    return success();
-  }
-
-#if 0
-  LogicalResult tagFunctionArguments(triton::FuncOp funcOp) {
-    MLIRContext *context = &getContext();
-
-    bool signalFailure = false;
-    funcOp.walk([&](Operation *op) {
-      Value ptr;
-      tt::IOType desired;
-      if (auto loadOp = dyn_cast<triton::LoadOp>(op)) {
-        ptr = loadOp.getPtr();
-        desired = tt::IOType::INPUT;
-      } else if (auto storeOp = dyn_cast<triton::StoreOp>(op)) {
-        ptr = storeOp.getPtr();
-        desired = tt::IOType::OUTPUT;
-      } else if (auto descLoad = dyn_cast<triton::DescriptorLoadOp>(op)) {
-        ptr = descLoad.getDesc();
-        desired = tt::IOType::INPUT;
-      } else if (auto descStore = dyn_cast<triton::DescriptorStoreOp>(op)) {
-        ptr = descStore.getDesc();
-        desired = tt::IOType::OUTPUT;
-      } else {
-        return;
-      }
-
-      BlockArgument arg = traceToFuncArg(ptr, funcOp);
-      if (!arg)
-        return;
-
-      unsigned idx = arg.getArgNumber();
-      if (failed(tagAs(idx, desired)))
-        signalFailure = true; // todo: uplevel this to the main runOnPOperation
-                              // function and just signalpassfailure here
-    });
-    return failed ? failure() : success();
-  }
-#endif
-
-  LogicalResult addTag(BlockArgument arg, triton::FuncOp funcOp) {
-    MLIRContext *context = &getContext();
-    unsigned idx = arg.getArgNumber();
-
-    // Tag the argument with `desired`, failing if it was already tagged with a
-    // conflicting io_type (i.e. used as both an input and an output).
-    auto tagAs = [&](tt::IOType desired) -> LogicalResult {
-      if (auto existingAttr = funcOp.getArgAttr(idx, kIOTypeAttrName)) {
-        auto existing = dyn_cast<tt::IOTypeAttr>(existingAttr);
-        if (existing && existing.getValue() == desired)
-          return success(); // already tagged consistently, no conflict
-        return funcOp.emitError()
-               << "argument " << arg << " (# " << idx
-               << ") is used as both an input and an output; conflicting "
-                  "io_type tag";
-      }
-      funcOp.setArgAttr(idx, kIOTypeAttrName,
-                        tt::IOTypeAttr::get(context, desired));
-      return success();
-    };
-
-#if 1
-    SetVector<Operation *> slice;
-    mlir::ForwardSliceOptions opts;
-    opts.filter = [&](Operation *op) {
-      if (isa<triton::DescriptorLoadOp, triton::DescriptorStoreOp>(op)) {
-        return llvm::is_contained(op->getOperands(), arg);
-      }
-      return true;
-    };
-    mlir::getForwardSlice(arg, &slice, opts);
-
-    for (auto *op : slice) {
-      if (isa<triton::LoadOp, triton::DescriptorLoadOp>(op)) {
-        if (failed(tagAs(tt::IOType::INPUT)))
-          return failure();
-      } else if (isa<triton::StoreOp, triton::DescriptorStoreOp>(op)) {
-        if (failed(tagAs(tt::IOType::OUTPUT)))
-          return failure();
-      }
-    }
-
-#else
-
-    SmallVector<Operation *> worklist;
-    SetVector<Operation *> visited;
-    worklist.append(arg.getUsers().begin(), arg.getUsers().end());
-    while (!worklist.empty()) {
-      Operation *op = worklist.back();
-      worklist.pop_back();
-      if (visited.contains(op))
-        continue;
-      visited.insert(op);
-
-      llvm::errs() << "visiting op " << *op << "\n";
-
-      // TODO: handle tensor descriptors separately
-      if (isa<triton::LoadOp>(op)) {
-        llvm::errs() << "found a load!\n";
-        assert(false && "TODO");
-      } else if (isa<triton::StoreOp>(op)) {
-        llvm::errs() << "found a store!\n";
-        assert(false && "TODO");
-      } else {
-        worklist.push_back(op);
-      }
-    }
-
-#endif
     return success();
   }
 };
