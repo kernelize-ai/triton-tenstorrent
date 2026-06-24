c08f016569bb1896a23cd136bada68cfcc77e018
Alex Baden <alex@kernelize.ai>
2026-06-24 20:37:24 +0000

    Support the full grid for tensor of ptr loads

--- npu/lib/Dialect/TritonTenstorrent/IR/Dialect.cpp
+++ npu/lib/Dialect/TritonTenstorrent/IR/Dialect.cpp
@@ -27,8 +27,19 @@ GridAttr TritonTenstorrentDialect::getGridAttr(ModuleOp mod) {
   if (auto attr = mod->getAttrOfType<GridAttr>(kAttrGridName))
     return attr;
 
-  // hardcode 1x1 grid for now
+#if 0
   return GridAttr::get(mod.getContext(), {1, 1});
+#else
+  // Read from the system descriptor if not present in the module, but store the
+  // grid attribute in the module so triton can modify the grid if required
+  // (e.g. shrinking the grid width for better multicast shapes)
+  auto deviceGrid = mlir::tt::ttcore::getCurrentScopeSystemDesc(mod)
+                        .getChipDescs()[0]
+                        .getGrid();
+  auto newGridAttr = GridAttr::get(mod.getContext(), deviceGrid);
+  mod->setAttr(kAttrGridName, newGridAttr);
+  return newGridAttr;
+#endif
 }
 
 // TODO: refactor to shared header
